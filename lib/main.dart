e673bdd09fb72586de07fcbf2105d7b71551dc0e
bertin.dev <bertmoun@yahoo.fr>
2022-08-01 03:49:40 +0100

    add forget interface in staging

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+import 'package:eido_events_project/pages/splash_screen.dart';
+import 'package:eido_events_project/routes/route_generators.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  //SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final navigatorKey = GlobalKey<NavigatorState>();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      title: "Eido Events",
+      initialRoute: SplashScreen.pageName,
+      onGenerateRoute: RouteGenerator.generateRoute,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          fontFamily: 'Inter',
+          disabledColor: Colors.white,
+          brightness: Brightness.light,
+          primaryColorDark: const Color(0xFF343434),
+          primaryColor: const Color(0xFF17506A),
+          // primaryColor: Color(0xFF068B68), SOACAM
+          // primaryColor: Color(0xFF00009C), Solina
+          primaryColorLight: Colors.white,
+          accentColor: const Color(0xFFBC054A),
+          // accentColor: Color(0xFF068B68), SOACAM
+          // accentColor: Color(0xFF00009C), Solina
+          buttonColor: Colors.white,
+          errorColor: Colors.red,
+          textTheme: TextTheme(
+              headline6: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+              subtitle2: TextStyle(color: Colors.grey[700])),
+          appBarTheme: const AppBarTheme(
+              color: Colors.black,
+              brightness: Brightness.dark,
+              textTheme: TextTheme(headline6: TextStyle(color: Colors.black)))),
+    );
+  }
+}
+
+
+
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:eido_events_project/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+import 'onbording_page.dart';
+
+//Page de chargement
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  static const String pageName = "splash_screen";
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5),
+            (){
+          Navigator.of(context, rootNavigator: true)
+              .popAndPushNamed(OnboardingPage.pageName);
+        }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final hv =MediaQuery.of(context).size.height/100;
+    final wv =MediaQuery.of(context).size.width/100;
+    return Scaffold(
+      backgroundColor: const Color(0xffD9532D),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/bg2.png'),
+              fit: BoxFit.cover,
+            )
+        ),
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  //width: wv*60,
+                  width: 162,
+                  height: 223,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+                const SizedBox(height: 10.0),
+                //Text('Chargement En Cours...', style:TextStyle(fontSize: 17.0, color: Colors.white.withOpacity(0.9), fontWeight: FontWeight.w600))
+              ],
+            ),
+            Positioned(
+                bottom: 0,
+                width: MediaQuery.of(context).size.width,
+                child: const Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Image(
+                    image: AssetImage('assets/images/bottom_bar.png'),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
